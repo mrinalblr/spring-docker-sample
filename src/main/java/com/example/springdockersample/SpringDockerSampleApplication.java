acc15363b459755780ad88221db75d7bade05adf
Deo <mrdeo@deloitte.com>
2019-08-19 12:23:49 +0530

    initial spring -boot code

---
+++ src/main/java/com/example/springdockersample/SpringDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerSampleApplication.class, args);
+	}
+
+}
